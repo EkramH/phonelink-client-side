1b9f6352c7b47f4fe01993ab6a8b5a0207d32544
EkramH <ekramhosen329@gmail.com>
2022-05-03 10:44:32 +0600

    header added and favicon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,37 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./Header.css";
+import logo from "../../assets/img/logo.png";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <>
+      <Navbar className="shadow-sm" bg="light" sticky="top" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img height="55" src={logo} alt="" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/service">
+                Inventory
+              </Nav.Link>
+              <Nav.Link as={Link} to="/blog">
+                Blog
+              </Nav.Link>
+              <Nav.Link as={Link} to="/blog">
+                Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
