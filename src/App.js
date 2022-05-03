b9817c31f0969ec4306220ef6f6b27c1bb596f15
EkramH <ekramhosen329@gmail.com>
2022-05-03 10:29:57 +0600

    bootstrap added and header footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Phonelink</h1>
+    <div>
+      <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
