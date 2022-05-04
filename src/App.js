5e34bc166901325b70050452cb2545955341eddc
EkramH <ekramhosen329@gmail.com>
2022-05-04 11:41:48 +0600

    inventory hooks

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import useInventory from "./hooks/useInventory";
+import Inventory from "./pages/Inventory/Inventory";
+
+export const InventoryContext = React.createContext([]);
 
 function App() {
+  const [inventories] = useInventory();
   return (
     <div>
       <Header></Header>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/home" element={<Home></Home>}></Route>
-      </Routes>
+      <InventoryContext.Provider value={inventories}>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="/inventory" element={<Inventory></Inventory>}></Route>
+        </Routes>
+      </InventoryContext.Provider>
       <Footer></Footer>
     </div>
   );
